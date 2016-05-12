977bac857c8edef73ee2bad927653d76251acb3d
gramman75 <gramman75@gmail.com>
2016-05-12 17:44:00 +0900

    20160512-01

--- study/temp.py
+++ study/temp.py
@@ -1,20 +1,27 @@
 #-*- coding:utf-8 -*-
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
-img = cv2.imread('images/logo.png')
+img = cv2.imread('images/perspective.jpg')
+rows, cols, ch = img.shape
 
-rows, cols = img.shape[:2]
+pts1 = np.float32([[455,725],[1698,183],[488,785],[1685,1280]])
+# pts1 = np.float32([[725,455],[183,1698],[785,488],[1280,1685]])
+pts2 = np.float32([[0,0],[1000,0],[0,1000],[1000,1000]])
 
-cv2.getRo
+# pts1의 좌표에 표시. Affine 변환 후 이동 점 확인.
+# cv2.circle(img, (200,100), 10, (255,0,0),-1)
+# cv2.circle(img, (400,100), 10, (0,255,0),-1)
+# cv2.circle(img, (200,200), 10, (0,0,255),-1)
 
-# 변환 행렬, X축으로 10, Y축으로 20 이동
-M = np.float32([[1,0,10],[0,1,20]])
+M = cv2.getPerspectiveTransform(pts1, pts2)
 
-dst = cv2.warpAffine(img, M,(cols, rows))
-cv2.imshow('Original', img)
-cv2.imshow('Translation', dst)
+dst = cv2.warpPerspective(img, M, (1000,1000))
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+plt.subplot(121),plt.imshow(img),plt.title('image')
+plt.subplot(122),plt.imshow(dst),plt.title('Affine')
+plt.show()
+
+
 
